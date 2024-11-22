7a63269aaf73e6cb52dc80b8caaf32623c656950
Debatty-Tom <145542891+Debatty-Tom@users.noreply.github.com>
2024-11-22 17:30:49 +0100

    create the member card component for the team page after seeded the database with some users

---
+++ app/Livewire/Team/MemberCard.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Livewire\Team;
+
+use App\Models\User;
+use Livewire\Component;
+
+class MemberCard extends Component
+{
+    public User $user;
+    public function mount($user)
+    {
+        $this->user = $user;
+    }
+    public function render()
+    {
+        return view('livewire.team.member-card');
+    }
+}
