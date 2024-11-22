c526910f38615840c03167e199fdd05b7f32652b
Debatty-Tom <145542891+Debatty-Tom@users.noreply.github.com>
2024-11-22 17:26:07 +0100

    add icons and layout for the icons

---
+++ resources/views/components/icons/layout.blade.php
@@ -0,0 +1,5 @@
+<svg viewBox="0 0 24 24"
+     xmlns="http://www.w3.org/2000/svg"
+     class="w-6">
+    {{ $slot }}
+</svg>
---
+++ resources/views/components/icons/stock.blade.php
@@ -0,0 +1,3 @@
+<x-icons.layout>
+    <path class="cls-1" d="M0,0h13.75v16.5H0V0ZM1.38,1.38v3.44h11V1.38H1.38ZM1.38,6.19v4.12h11v-4.12H1.38ZM1.38,11.69v3.44h11v-3.44H1.38Z"/>
+</x-icons.layout>
