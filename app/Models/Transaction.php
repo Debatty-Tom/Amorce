7916eb28bcb084e7f6bc99f4db35a0b2abb8ae70
Debatty-Tom <145542891+Debatty-Tom@users.noreply.github.com>
2024-11-29 17:25:19 +0100

    add migration, factory and model for the transactions

---
+++ app/Models/Transaction.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Notifications\Notifiable;
+
+class Transaction extends Model
+{
+    /** @use HasFactory<\Database\Factories\TransactionFactory> */
+    use HasFactory, Notifiable;
+
+
+    protected $fillable = [
+        'title',
+        'description',
+        'amount',
+        'date',
+    ];
+
+    protected $casts = [
+        'date' => 'datetime:Y-m-d',
+    ];
+
+    public function fund() : BelongsTo
+    {
+        return $this->belongsTo(Fund::class);
+    }
+}
---
+++ database/factories/TransactionFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Transaction>
+ */
+class TransactionFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title'=> $this->faker->words(3, true),
+            'description'=> $this->faker->words(4, true),
+            'amount'=> $this->faker->numberBetween(0, 500000),
+            'date'=> $this->faker->dateTimeBetween('-1 year','2 year')
+        ];
+    }
+}
