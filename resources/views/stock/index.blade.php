fff753aea1263889b76c5b2527bca65abe35b865
Debatty-Tom <145542891+Debatty-Tom@users.noreply.github.com>
2024-11-29 17:24:34 +0100

    update the header for stock page

--- resources/views/stock/index.blade.php
+++ resources/views/stock/index.blade.php
@@ -1,7 +1,13 @@
 <x-app-layout>
-    <h1>
-        {{ __('Hello stock') }}
-    </h1>
+    <div class="flex justify-between items-center mb-8">
+        <h1 class="font-bold text-2xl">
+            {{ __('Stock') }}
+        </h1>
+        <a href="{{ route('team.create') }}"
+           class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            {{ __('Add a folder') }}
+        </a>
+    </div>
     <ul class="grid grid-cols-5 gap-9 w-full p-8">
         @foreach($folders as $folder)
             <li class="flex justify-center">
