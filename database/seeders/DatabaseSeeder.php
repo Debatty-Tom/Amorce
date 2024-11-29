07810a78fb8eaacc72622f64bde52b100b5dff31
Debatty-Tom <145542891+Debatty-Tom@users.noreply.github.com>
2024-11-29 17:30:06 +0100

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,9 @@
 namespace Database\Seeders;
 
 use App\Models\Fund;
+use App\Models\Project;
 use App\Models\Stock;
+use App\Models\Transaction;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,11 +17,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
 
-        User::factory(6)->create([]);
-        Stock::factory(6)->create([]);
-        Fund::factory(6)->create([]);
+        User::factory(6)
+            ->hasTodos(6)
+            ->create();
+        Stock::factory(6)->create();
+        Fund::factory(6)
+            ->hasTransactions(6)
+            ->create();
+        Project::factory(6)->create();
+
         User::factory()->create([
             'name' => 'admin',
             'email' => 'tom.debatty@hotmail.be',
