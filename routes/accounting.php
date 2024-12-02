d699c122a1427b69e7974bdb62dcb2e65097896d
Debatty-Tom <145542891+Debatty-Tom@users.noreply.github.com>
2024-12-02 17:14:31 +0100

    refactor accounting routes and add transactions routes

--- routes/accounting.php
+++ routes/accounting.php
@@ -1,10 +1,15 @@
 <?php
 
+use App\Livewire\Accounting\FundsTable;
+use App\Livewire\Accounting\FundTable;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('/accounting', [App\Http\Controllers\AccountingController::class, 'index'])
+    Route::get('/accounting', FundsTable::class)
         ->name('accounting.index');
-    Route::get('/accounting/create', [App\Http\Controllers\AccountingController::class, 'create'])
+    Route::get('/accounting/create', FundsTable::class)
         ->name('accounting.create');
+
+    Route::get('/accounting/{fund}', FundTable::class)
+        ->name('accounting.show');
 });
