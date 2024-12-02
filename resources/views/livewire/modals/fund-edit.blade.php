a592f431b3dd61cfe8f43455580055a105ffe5e7
Debatty-Tom <145542891+Debatty-Tom@users.noreply.github.com>
2024-12-02 17:15:22 +0100

    add modals

---
+++ resources/views/livewire/modals/fund-edit.blade.php
@@ -0,0 +1,4 @@
+<x-modals.layout>
+    <x-slot name="maxWidth">max-w-screen-xl</x-slot>
+</x-modals.layout>
+
---
+++ resources/views/livewire/transactions/transactions-table.blade.php
@@ -0,0 +1,52 @@
+<div>
+    <table class="w-full bg-white rounded mb-4">
+        <thead>
+        <tr class="bg-gray-200">
+            <th class="text-left  p-4">
+                {{ __('Date') }}
+            </th>
+            <th class="text-left">
+                {{ __('Description') }}
+            </th>
+            <th class="text-left">
+                {{ __('Amount') }}
+            </th>
+            <th class="text-left">
+                {{ __('edit/delete') }}
+            </th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach($this->transactions as $transaction)
+            <tr>
+                <td class=" p-4">
+                    {{ date_format(($transaction->date), 'd/m/Y') }}
+                </td>
+                <td>
+                    {{ $transaction->description }}
+                </td>
+                <td>
+                    @if (strpos($transaction->amount, '-') !== false)
+                        <p class="text-red-500">
+                            {{ number_format(($transaction->amount/100),2, ',',' ')."€" }}
+                        </p>
+                    @else
+                        <p class="text-green-400">
+                            {{ number_format(($transaction->amount/100),2, ',',' ')."€" }}
+                        </p>
+                    @endif
+                </td>
+                <td>
+                    <a href="{{ route('transactions.edit', $fund->id, $transaction->id,) }}">
+                        {{ __('edit') }}
+                    </a>
+                    <a href="{{ route('transactions.destroy', $transaction->id, $fund->id) }}">
+                        {{ __('delete') }}
+                    </a>
+                </td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+    {{ $this->transactions->links() }}
+</div>
