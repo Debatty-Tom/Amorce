592d6e2e44f523d3b4cf9726f4339b4cac2fce7b
Debatty-Tom <145542891+Debatty-Tom@users.noreply.github.com>
2025-01-16 00:19:19 +0100

    refactor draw part

--- resources/views/livewire/draws/draws-list.blade.php
+++ resources/views/livewire/draws/draws-list.blade.php
@@ -1,20 +1,39 @@
-<div class="flex w-full">
-    <ul class="p-4 flex-row flex-wrap w-full">
-        @foreach($draws as $draw)
-            <li class="relative p-8 bg-white flex gap-10 m-2.5 rounded w-full" wire:key="{{$draw->id}}">
-                <a href="{{ route('draw.show',$draw->id)}}" class="inset-0 absolute"></a>
-                {{ date_format(($draw->date), 'd/m/Y') }}
+<ul class="p-4 grid grid-cols-4 gap 4">
+    @foreach($draws as $draw)
+        <li class="relative p-8 bg-white flex flex-col gap-5 m-2.5 rounded" wire:key="{{$draw->id}}">
+            <a href="{{ route('draw.show',$draw->id)}}" class="inset-0 absolute"></a>
+            <div class="flex flex-row gap-10">
+                <div>
+                    <p>
+                        {{__('Date :')}}
+                    </p>
+                    <p>
+                        {{ date_format(($draw->date), 'd/m/Y') }}
+                    </p>
+                </div>
+                <div>
+                    <p>
+                        {{__('Amount :')}}
+                    </p>
+                    <p>
+                        {{ number_format(($draw->amount/100),2, ',',' ')."€" }}
+                    </p>
+                </div>
+            </div>
+            <div>
                 <p>
-                    {{ number_format(($draw->amount/100),2, ',',' ')."€" }}
+                    {{__('Projects :')}}
                 </p>
-                <ul>
+                <ul class="pl-3">
                     @foreach($draw->projects as $project)
-                        <li>
-                            {{ $project->title }}
+                        <li class="list-disc">
+                            <p>
+                                {{$project->title }}
+                            </p>
                         </li>
                     @endforeach
                 </ul>
-            </li>
-        @endforeach
-    </ul>
-</div>
+            </div>
+        </li>
+    @endforeach
+</ul>
