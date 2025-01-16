f952ede0b1ea00c56280c6a01109e080bc64ddb0
Debatty-Tom <145542891+Debatty-Tom@users.noreply.github.com>
2025-01-16 11:48:42 +0100

    add select projects in draw create

--- resources/views/livewire/modals/draw-create.blade.php
+++ resources/views/livewire/modals/draw-create.blade.php
@@ -45,7 +45,7 @@
         </div>
         <div class="flex justify-between">
             <p class="pb-8 pr-6 w-full lg:w-1/2">
-                {{ __("The 3 nex participants") }}
+                {{ __("The 3 next participants :") }}
             </p>
             @if(count($this->form->new_participants) === 0)
                 <button wire:click.prevent="randomParticipants"
@@ -53,18 +53,35 @@
                     {{ __("Start random") }}
                 </button>
             @endif
-
+            <ul class="grid grid-cols-3 gap-5">
+                @if(count($this->form->new_participants) > 0)
+                    @foreach($this->form->new_participants as $participant)
+                        <li wire:key="{{ $participant->id }}" class="bg-slate-100 shadow-md rounded-lg p-6 flex flex-col justify-between items-start">
+                            <p class="text-lg font-semibold text-gray-800">
+                                {{ $participant->name }}
+                            </p>
+                        </li>
+                    @endforeach
+                @endif
+            </ul>
         </div>
-        <ul class="grid grid-cols-3">
-            @if(count($this->form->new_participants) > 0)
-                @foreach($this->form->new_participants as $participant)
-                    <li wire:key="{{ $participant->id }}">
-                        <p>
-                            {{ $participant->name }}
-                        </p>
-                    </li>
-                @endforeach
-            @endif
+        <p>
+            {{ __("Select the projects you want in this draw") }}
+        </p>
+        <ul class="grid grid-cols-3 gap-5">
+            @foreach ($projects as $project)
+                <li class="w-full bg-slate-100 shadow-md rounded-lg flex flex-col justify-between items-start" wire:key="{{ $project->id }}">
+                    <div class="flex items-center gap-4">
+                        <input id="{{ $project->id }}"
+                               type="checkbox"
+                               name="{{ __("projects[]") }}"
+                               wire:model="selectedProjects"
+                               class="h-4 w-4 ml-6 text-indigo-600 border-gray-300 rounded focus:ring-indigo-500"
+                               value="{{ $project->id }}">
+                        <label for="{{ $project->id }}" class="w-full pt-6 pb-6 pr-6 text-lg font-semibold text-gray-800">{{ $project->title }}</label>
+                    </div>
+                </li>
+            @endforeach
         </ul>
         <div class="flex justify-end">
             <button
