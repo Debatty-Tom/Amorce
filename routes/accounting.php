ccc5cd040c4344284f8e299de0fec66f408c405d
Debatty-Tom <145542891+Debatty-Tom@users.noreply.github.com>
2025-01-16 20:04:34 +0100

    update routes

--- routes/accounting.php
+++ routes/accounting.php
@@ -2,6 +2,7 @@
 
 use App\Livewire\Accounting\FundsTable;
 use App\Livewire\Accounting\FundTable;
+use App\Livewire\Csv\ImportCsv;
 use App\Models\TransactionSummaryView;
 use Illuminate\Support\Facades\Route;
 
@@ -13,4 +14,7 @@ Route::middleware(['auth', 'verified'])->group(function () {
 
     Route::get('/accounting/{fund_id}', FundTable::class)
         ->name('accounting.show');
+
+    Route::get('/csv', ImportCsv::class)
+        ->name('csv.index');
 });
