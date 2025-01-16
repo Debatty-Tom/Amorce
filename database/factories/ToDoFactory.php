de19591ca13e30d093d52d4c3b58bcb3da94a126
Debatty-Tom <145542891+Debatty-Tom@users.noreply.github.com>
2025-01-16 20:06:25 +0100

    add import csv button

--- database/factories/ToDoFactory.php
+++ database/factories/ToDoFactory.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Todo>
  */
-class ToDoFactory extends Factory
+class TodoFactory extends Factory
 {
     /**
      * Define the model's default state.
