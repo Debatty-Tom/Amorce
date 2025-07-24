19fa933b0dd8e890a1f947383a61ba2275a51bfe
Debatty-Tom <145542891+Debatty-Tom@users.noreply.github.com>
2025-07-24 23:21:14 +0200

    add archived fund on fundtable

--- app/Livewire/Accounting/FundsTable.php
+++ app/Livewire/Accounting/FundsTable.php
@@ -3,6 +3,7 @@
 namespace App\Livewire\Accounting;
 
 use App\Models\Fund;
+use App\Models\TransactionSummaryView;
 use Illuminate\Support\Facades\DB;
 use Livewire\Attributes\Computed;
 use Livewire\Component;
@@ -11,14 +12,20 @@ class FundsTable extends Component
 {
     #[Computed]
     public function principalFunds(){
-        return DB::table('transaction_summary_view')->where('fund_type', 'principal')->get();
+        return DB::table('transaction_summary_view')->whereNull('deleted_at')->where('fund_type', 'principal')->get();
 
     }
     #[Computed]
     public function specificFunds(){
-        return DB::table('transaction_summary_view')->where('fund_type', 'specific')->get();
+        return DB::table('transaction_summary_view')->whereNull('deleted_at')->where('fund_type','specific')->get();
 
     }
+    #[Computed]
+    public function archivedFunds()
+    {
+        return DB::table('transaction_summary_view')->whereNotNull('deleted_at')->get();
+    }
+
 
     public function render()
     {
