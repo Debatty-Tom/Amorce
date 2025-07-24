2b7ec0e743a5acfd31d0c094fd8bc2ae3f633019
Debatty-Tom <145542891+Debatty-Tom@users.noreply.github.com>
2025-07-24 15:21:57 +0200

    add confirm delete feature

---
+++ resources/views/components/cancel-button.blade.php
@@ -0,0 +1,12 @@
+@props([
+    'click' => null
+])
+
+<button
+    type="button"
+    class="py-2 px-4 bg-amber-400 rounded"
+    @if($click)
+        wire:click="{{ $click }}"
+    @endif >
+    Annuler
+</button>
