c1e6bad7f25da5154ef786de327751248a51ae01
Debatty-Tom <145542891+Debatty-Tom@users.noreply.github.com>
2025-07-24 15:18:01 +0200

    fix console error $livewireComponent not found

--- app/Livewire/Layout/WireElementModal.php
+++ app/Livewire/Layout/WireElementModal.php
@@ -10,15 +10,15 @@ class WireElementModal extends Component
     public $isOpen = false;
     public $livewireComponent = null;
 
-    protected $listeners = ['openModal'];
+    protected $listeners = [
+        'openModal',
+    ];
 
     #[On('openModal')]
     public function openModal($component, $params = null)
     {
         $this->livewireComponent = $component;
         $this->isOpen = true;
-
-        $this->dispatch('open-modal',$component, $params);
     }
 
     #[On('closeModal')]
@@ -27,6 +27,7 @@ class WireElementModal extends Component
         $this->isOpen = false;
         $this->livewireComponent = null;
     }
+
     public function render()
     {
         return view('livewire.layout.wire-element-modal');
--- resources/views/livewire/layout/wire-element-modal.blade.php
+++ resources/views/livewire/layout/wire-element-modal.blade.php
@@ -1,6 +1,6 @@
 <div
     id="wire-element-modal"
-    x-data="{ open: @entangle('isOpen')}"
+    x-data="{ open: @entangle('isOpen'), livewireComponent: @entangle($livewireComponent)}"
     x-show="open"
     x-cloak
     class="fixed inset-0 bg-black bg-opacity-50 z-40 center"
@@ -12,7 +12,6 @@
     x-transition:leave-start="opacity-100"
     x-transition:leave-end="opacity-0"
 >
-    <!-- Modal Content -->
     <div
         class="fixed inset-y-0 right-0 w-1/2 bg-white shadow-lg z-50 flex flex-col transform"
         x-show="open"
@@ -23,7 +22,6 @@
         x-transition:leave-start="translate-x-0"
         x-transition:leave-end="translate-x-full"
     >
-        <!-- Close Button -->
         <div class="flex justify-end p-4">
             <span wire:click="dispatch('closeModal')"
                   class="text-gray-600 hover:text-black">
@@ -33,9 +31,13 @@
                 </svg>
             </span>
         </div>
-        <div x-show="$livewireComponent" class="p-6">
+        <div x-show="livewireComponent" class="p-6">
             @if($livewireComponent)
-                <livewire:dynamic-component :component="$livewireComponent" :key="$livewireComponent" />
+                <livewire:dynamic-component
+                    :component="$livewireComponent"
+                    :key="$livewireComponent"
+                    :params="$componentParams ?? []"
+                />
             @endif
         </div>
     </div>
