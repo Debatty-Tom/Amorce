93344ba6ab4f619c04ba00a0241cb735d8c56d55
Debatty-Tom <145542891+Debatty-Tom@users.noreply.github.com>
2025-07-24 15:05:00 +0200

    add money brick package

--- app/Livewire/Accounting/FundCard.php
+++ app/Livewire/Accounting/FundCard.php
@@ -3,6 +3,8 @@
 namespace App\Livewire\Accounting;
 
 use App\Models\Fund;
+use Brick\Money\Money;
+use Illuminate\Support\Str;
 use Livewire\Component;
 
 class FundCard extends Component
@@ -11,6 +13,10 @@ class FundCard extends Component
     public function mount($fund)
     {
         $this->fund = $fund;
+
+        $fund->descriptionLimited = str::limit($fund->fund_description, 50, preserveWords: true);
+        $fund->amount = Money::ofMinor($fund->total_amount, 'EUR')->formatTo('fr_BE');
+        $fund->titleLimited = str::limit($fund->fund_title, 25, preserveWords: true);
     }
     public function render()
     {
