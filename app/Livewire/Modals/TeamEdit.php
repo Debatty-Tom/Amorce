b2fa1aae7e455d3851a6acfa4a2e1dd84c354356
Debatty-Tom <145542891+Debatty-Tom@users.noreply.github.com>
2025-07-24 15:23:25 +0200

    update teamedit for confirm delete feature, handle card modal, and toast feature

--- app/Livewire/Modals/TeamEdit.php
+++ app/Livewire/Modals/TeamEdit.php
@@ -4,25 +4,52 @@ namespace App\Livewire\Modals;
 
 use App\Livewire\Forms\TeamForm;
 use App\Models\User;
+use App\Traits\handlesImagesUpload;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class TeamEdit extends Component
 {
+    use WithFileUploads, handlesImagesUpload;
     public $feedback = '';
-    public User $user;
+    public $user;
     public TeamForm $form;
+    public $showDeleteModal = false;
     public $loading;
     public function mount(User $user)
     {
         $this->form->setUser($user);
-
-        $this->user = $user;
+        $this->user = User::find($user['id']);
     }
     public function save(){
         $this->form->update();
         $this->feedback='User updated successfully';
 
-        $this->dispatch('closeModal');
+        $this->dispatch('closeCardModal');
+        $this->dispatch(event:'openalert', params:['message' => $this->feedback]);
+        $this->dispatch('refresh-users');
+
+    }
+    public function confirmDelete()
+    {
+        $this->showDeleteModal = true;
+    }
+
+    public function cancelDelete()
+    {
+        $this->showDeleteModal = false;
+    }
+
+    public function deleteTeam()
+    {
+        $this->user->delete();
+
+        $this->feedback='User deleted successfully';
+
+        $this->dispatch('refresh-users');
+        $this->showDeleteModal = false;
+        $this->dispatch('closeCardModal');
+        $this->dispatch(event:'openalert', params:['message' => $this->feedback]);
     }
 
     public function render()
