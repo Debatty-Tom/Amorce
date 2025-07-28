7bddbc4fed9dcded889ef9b470241234408cc409
Debatty-Tom <145542891+Debatty-Tom@users.noreply.github.com>
2025-07-28 21:07:42 +0200

    add to do edit

---
+++ app/Livewire/Modals/TodoShow.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Livewire\Modals;
+
+use App\Livewire\Forms\TodoForm;
+use App\Models\Todo;
+use App\Traits\DeleteModalTrait;
+use Livewire\Component;
+
+class TodoShow extends Component
+{
+    use DeleteModalTrait;
+    public TodoForm $form;
+    public $project;
+    public $feedback = '';
+    public $todo;
+
+    public function mount(Todo $todo)
+    {
+        $this->form->setTodo($todo);
+        $this->todo = Todo::with(['assignments.assignedBy'])->find($todo['id']);
+    }
+    public function deleteTodo()
+    {
+        $this->todo->delete();
+
+        $this->feedback='Todo deleted successfully';
+
+        $this->dispatch('refresh-todos');
+        $this->showDeleteModal = false;
+        $this->dispatch('closeCardModal');
+        $this->dispatch(event:'openalert', params:['message' => $this->feedback]);
+    }
+    public function render()
+    {
+        return view('livewire.modals.todo-show');
+    }
+}
