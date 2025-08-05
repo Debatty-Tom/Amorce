138c016a996fd7d33017a5190631dcdf1bf6c2cb
Debatty-Tom <145542891+Debatty-Tom@users.noreply.github.com>
2025-08-05 14:31:36 +0200

    fix dashboard if no draw or todo planned

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -3,12 +3,14 @@
     <section class="w-full h-full flex flex-row justify-between gap-8">
         <livewire:calendar>
         </livewire:calendar>
-        <div class="w-full max-w-72 flex flex-col gap-6">
+        <div class="w-full max-w-80 flex flex-col gap-6">
+            @php
+                $redirect = $this->draw ? route('draw.show', ['id' => $this->draw->id]) : null;
+            @endphp
             <x-dashboard-card
                 icon="icons.nine"
                 text="{{ $this->draw?->date?->format('d/m/Y') ?? 'Il n’y a pas de réunion prévue prochainement' }}"
-                redirect="{{ route('draw.show', ['id' => $this->draw->id]) }}"
-            >
+                redirect="{{$redirect}}">
                 Prochaine réunion
             </x-dashboard-card>
             <x-dashboard-card
@@ -16,10 +18,15 @@
                 text="{{ $this->todo?->date?->format('d/m/Y') ?? 'Il n’y a pas d’événement prévu prochainement' }}">
                 Prochain événement
             </x-dashboard-card>
+            @php
+                $click = $this->todo
+                    ? "dispatch('openCardModal', {component: 'modals.todo-show', params: { id: {$this->todo->id} }})"
+                    : null;
+            @endphp
             <x-dashboard-card
                 icon="icons.todo"
                 text="{{ $this->todo?->date?->format('d/m/Y') ?? 'Vous avez éffectué toutes vos tâches'}}"
-                click="$dispatch('openCardModal',{component: 'modals.todo-show', params: { id: {{ $this->todo->id }} }})">
+                :click="$click">
                 Prochaine tâche
             </x-dashboard-card>
         </div>
