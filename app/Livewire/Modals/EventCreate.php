f78080285eddfaf24905c2a48ff2e7d4cb5bfb7b
Debatty-Tom <145542891+Debatty-Tom@users.noreply.github.com>
2025-08-06 10:16:47 +0200

    add a show and create feature for events

---
+++ app/Livewire/Modals/EventCreate.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Livewire\Modals;
+
+use App\Livewire\Forms\EventForm;
+use App\Models\Event;
+use Illuminate\Support\Carbon;
+use Livewire\Component;
+
+class EventCreate extends Component
+{
+    public string $feedback;
+    public EventForm $form;
+    public function mount(Event $event, $date)
+    {
+        $this->form->setEvent($event);
+        $this->form->date = Carbon::parse($date)->toDateString();
+    }
+
+    public function save()
+    {
+        $this->form->create();
+        $this->feedback='Event created successfully';
+
+        $this->dispatch('closeCardModal');
+        $this->dispatch(event: 'openalert', params: ['message' => $this->feedback]);
+        $this->dispatch(event: 'refresh-calendar');
+    }
+    public function render()
+    {
+        return view('livewire.modals.event-create');
+    }
+}
