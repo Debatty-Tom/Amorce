7face6aa013752938b85846efc0a10472afdb60f
Debatty-Tom <145542891+Debatty-Tom@users.noreply.github.com>
2025-08-18 19:48:22 +0200

    fix navigation index

--- resources/views/livewire/layout/navigation.blade.php
+++ resources/views/livewire/layout/navigation.blade.php
@@ -1,4 +1,4 @@
-<nav class="fixed top-0 left-0 h-full w-80 bg-white shadow z-50 p-4">
+<nav class="fixed top-0 left-0 h-full w-80 bg-white shadow p-4">
     <h1 class="justify-self-center p-4 mt-5 font-bold text-3xl text-center">{{ __('amorce.navigation-title') }}</h1>
     <ul class="flex flex-col gap-2 justify-center p-8">
         @foreach ($links as $link)
