dd9851e45b393d1c8e750a303e27ef880e721752
Debatty-Tom <145542891+Debatty-Tom@users.noreply.github.com>
2025-08-24 18:12:07 +0200

    fix team card spa

--- app/Livewire/Team/MemberCard.php
+++ app/Livewire/Team/MemberCard.php
@@ -3,15 +3,29 @@
 namespace App\Livewire\Team;
 
 use App\Models\User;
+use Livewire\Attributes\Computed;
+use Livewire\Attributes\On;
 use Livewire\Component;
 
 class MemberCard extends Component
 {
+    public $userId;
     public User $user;
 
     public function mount($user)
     {
-        $this->user = $user;
+        $this->userId = $user->id;
+    }
+    #[Computed]
+    public function user()
+    {
+        return User::find($this->userId);
+    }
+
+    #[on('refresh-users')]
+    public function refreshUser()
+    {
+        return;
     }
 
     public function render()
